4772335a7dc01cdf80ebd79823d0660765012c28
RujutaDabke <rujutadabke25@gmail.com>
2021-09-12 00:47:15 +0530

    Variable

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int num = 10;
+        System.out.println(" Num = "+num);
+        num = 2 * num;
+        System.out.println("Value of num is multiplied by 2 ");
+        System.out.println(num);
+    }
+}
